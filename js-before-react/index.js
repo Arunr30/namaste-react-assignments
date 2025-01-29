36d9bbfcbb5580c709d190bd338365bcd2b93a04
Arunvasuacchu <arunvasur@gmail.com>
2025-01-29 20:39:32 +0530

    array and obj destructuring

---
+++ js-before-react/index.js
@@ -0,0 +1,39 @@
+// js before react js
+
+
+// logical and (&&) checks both value if one is false everything is false.,
+// logical OR (||) either one is true returns true.,
+
+
+// template literals
+
+let name1 = "Arun"
+let name2 = "Vasu"
+
+
+// console.log(`your first name is ${name1} and your second name is ${name2}`)
+
+// object destructuring
+
+const obj = {
+  id: 1,
+  title: 'hi',
+  desc: "hello world"
+}
+
+// const getId = obj.id
+// console.log(getId)
+// const {id, title} = obj
+// console.log(id)
+// console.log(title)
+
+
+// array destructuring
+
+
+const arr = [1,2,3];
+
+const [firstElem, secondElem, thirdElem, fourthElem] = arr
+console.log(firstElem, secondElem, thirdElem, fourthElem) // --> when we are accessing an element that not present in an array it will give undefined
+
+
