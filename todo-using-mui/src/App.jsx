9fdf4d431ad270307dff8a4e3ef9009b86b9274c
Arunvasuacchu <arunvasur@gmail.com>
2025-03-04 19:00:21 +0530

    todo using mui

--- todo-using-mui/src/App.jsx
+++ todo-using-mui/src/App.jsx
@@ -1,35 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect } from "react";
+import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [todo, setTodo] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
 
+  async function getTodoList() {
+    try {
+      setLoading(true);
+      const todoResult = await fetch("https://dummyjson.com/todos");
+      const response = await todoResult.json();
+      console.log(response);
+      setTodo(response);
+    } catch (e) {
+      setError("error", e);
+    }
+  }
+  useEffect(() => {
+    getTodoList();
+  }, []);
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>todo app using mui</h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
